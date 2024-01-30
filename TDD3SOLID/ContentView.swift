ecb43ffab09c23fb13aaaea94622ea2780e99af1
Austin Dobberfuhl <AUSTIN.DOBBERFUHL4597@STU.MTEC.EDU>
2024-01-30 08:25:43 -0700

    Initial Commit

---
+++ TDD3SOLID/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TDD3SOLID
+//
+//  Created by Austin Dobberfuhl on 1/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TDD3SOLID/TDD3SOLIDApp.swift
@@ -0,0 +1,17 @@
+//
+//  TDD3SOLIDApp.swift
+//  TDD3SOLID
+//
+//  Created by Austin Dobberfuhl on 1/30/24.
+//
+
+import SwiftUI
+
+@main
+struct TDD3SOLIDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
